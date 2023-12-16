19e78cee68bd78f154aa21d6008c0be718579c6c
张文皓 <zhangwh754@163.com>
2023-12-16 20:05:37 +0800

    style: prettier格式化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Next博客",
-  description: "Next+tailwindcss全栈博客",
-};
+  title: 'Next博客',
+  description: 'Next+tailwindcss全栈博客',
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN">
       <body>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <main>
       <h2 className="text-3xl text-red-600 font-bold">Hello World</h2>
     </main>
-  );
+  )
 }
